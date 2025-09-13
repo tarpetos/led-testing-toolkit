2ad17268201c862fce7c1a70281ff513ea166f5c
tarpetos <hevlichtaras@gmail.com>
2025-09-14 00:17:04 +0300

    Replace timestamp with absolute LEDs time

--- led_testing_toolkit/led_models.py
+++ led_testing_toolkit/led_models.py
@@ -1,40 +1,38 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import TYPE_CHECKING
 
 from pydantic import field_validator
 from pydantic.dataclasses import dataclass
 
-if TYPE_CHECKING:
-    from collections.abc import Sequence
+from collections.abc import Sequence
 
 
 @dataclass
 class Color:
-    R: int  # 0-255
-    G: int  # 0-255
-    B: int  # 0-255
+    R: int
+    G: int
+    B: int
 
-
-@dataclass
-class LED:
-    timestamp: datetime
-    rel_time: float  # 0, 1.5, 2.5, ...
-    color: Color  # Color(0, 255, 0), Color(0, 25, 23), Color(13, 55, 2), ...
-
-    @field_validator("timestamp", mode="before")
+    @field_validator("R", "G", "B")
     @classmethod
-    def validate_timestamp(cls, v: datetime | str) -> datetime:
-        if not isinstance(v, datetime):
-            raise TypeError("field `timestamp` must be an instance `datetime`:")
+    def validate_rgb(cls, v):
+        if not 0 <= v <= 255:
+            raise TypeError("RGB values must be between 0 and 255:")
         return v
 
 
 @dataclass
+class LED:
+    rel_time: float
+    color: Color
+    abs_time: float
+
+
+@dataclass
 class LEDSequence:
-    number: int  # 1, 2, 3, 4, ...
-    leds: Sequence[LED]  # [LED(rel_time=0.0, color=Color(0, 255, 0)), LED(rel_time=0.0, color=Color(0, 255, 0)), ...]
+    number: int
+    leds: Sequence[LED]
 
 
 @dataclass
